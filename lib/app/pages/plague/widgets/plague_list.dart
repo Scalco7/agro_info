311c8389ba4d85fa2a7090c29e37dd128c517ae0
Felipe Scalco <fscalco7@gmail.com>
2026-03-10 17:35:45 -0300

    feat: add loading animation

--- lib/app/pages/plague/widgets/plague_list.dart
+++ lib/app/pages/plague/widgets/plague_list.dart
@@ -1,6 +1,7 @@
 import 'package:agro_info/app/common/providers/plague_provider.dart';
 import 'package:agro_info/app/pages/plague/widgets/plague_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 class PlagueList extends StatelessWidget {
@@ -13,12 +14,31 @@ class PlagueList extends StatelessWidget {
       child: Consumer<PlagueProvider>(
         builder: (_, plagueProvider, widget) {
           if (plagueProvider.plagues == null) {
-            return Center(child: Text("Erro ao carregar pragas!"));
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 10,
+              children: [
+                Lottie.asset(
+                  "assets/animations/inchworm_animation.json",
+                  fit: BoxFit.contain,
+                  alignment: AlignmentGeometry.center,
+                  repeat: true,
+                  width: 200,
+                ),
+                Text(
+                  "Carregando...",
+                  style: TextStyle(fontWeight: FontWeight(500), fontSize: 16),
+                ),
+              ],
+            );
           } else if (plagueProvider.plagues!.isEmpty) {
             return Center(child: Text("carregando pragas!"));
           } else {
             return GridView.builder(
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 150),
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 150,
+              ),
               itemCount: plagueProvider.plagues!.length,
               padding: EdgeInsets.only(bottom: 20, left: 20, right: 20, top: 0),
               itemBuilder: (context, index) =>

026562ffd824ec09fa58b484309a1c0b292ad12e
Felipe Scalco <fscalco7@gmail.com>
2026-03-11 11:44:30 -0300

    feat: create calculation form

--- lib/app/pages/planting_zone/planting_zone_page.dart
+++ lib/app/pages/planting_zone/planting_zone_page.dart
@@ -1,5 +1,6 @@
 import 'package:agro_info/app/common/widgets/app_bar.dart';
 import 'package:agro_info/app/common/widgets/bottom_navigation_bar.dart';
+import 'package:agro_info/app/pages/planting_zone/widgets/calculation_form.dart';
 import 'package:flutter/material.dart';
 
 class PlantingZonePage extends StatelessWidget {
@@ -8,17 +9,20 @@ class PlantingZonePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: 'Risco de Insucesso baseado no ZARC', icon: Icons.agriculture_outlined),
+      appBar: CustomAppBar(
+        title: 'Calculo de Risco',
+        icon: Icons.agriculture_outlined,
+      ),
       backgroundColor: Theme.of(context).colorScheme.surface,
       bottomNavigationBar: CustomBottomNavigationBar(index: 1),
       body: Padding(
-        padding: const EdgeInsets.only(top: 40),
-        child: Column(
-          spacing: 20,
-          children: [
-           
-          ],
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 10,
+          right: 10,
+          bottom: 20,
         ),
+        child: CalculationForm(),
       ),
     );
   }
---
+++ lib/app/pages/planting_zone/widgets/calculation_form.dart
@@ -0,0 +1,102 @@
+import 'package:agro_info/app/common/enums/state_enum.dart';
+import 'package:agro_info/app/common/widgets/app_dropdown_menu.dart';
+import 'package:flutter/material.dart';
+
+class CalculationForm extends StatefulWidget {
+  const CalculationForm({super.key});
+
+  @override
+  State<CalculationForm> createState() => _CalculationFormState();
+}
+
+class _CalculationFormState extends State<CalculationForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      spacing: 40,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Calculo do Risco de Plantio",
+              style: TextStyle(fontWeight: FontWeight(600), fontSize: 20),
+            ),
+            Text(
+              "Calculo Baseado no Zoneamento Agrícola de Risco Climático",
+              style: TextStyle(fontWeight: FontWeight(400), fontSize: 14),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 26,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 20,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: AppDropdownMenu(
+                    label: Text("Estado"),
+                    hintText: "Selecione o Estado",
+                    dropdownMenuEntries: StateEnum.values
+                        .map(
+                          (state) => DropdownMenuEntry<StateEnum>(
+                            value: state,
+                            label: state.name,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: AppDropdownMenu(
+                    label: Text("Cidades"),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry<String>(
+                        label: "Test 1",
+                        value: "Teste 1",
+                      ),
+                      DropdownMenuEntry<String>(
+                        label: "Test 2",
+                        value: "Teste 2",
+                      ),
+                      DropdownMenuEntry<String>(
+                        label: "Test 3",
+                        value: "Teste 3",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            AppDropdownMenu(
+              label: Text("Culturas"),
+              dropdownMenuEntries: [
+                DropdownMenuEntry<String>(label: "Test 1", value: "Teste 1"),
+                DropdownMenuEntry<String>(label: "Test 2", value: "Teste 2"),
+                DropdownMenuEntry<String>(label: "Test 3", value: "Teste 3"),
+              ],
+            ),
+            AppDropdownMenu(
+              label: Text("Risco"),
+              suffix: Text("%"),
+              dropdownMenuEntries: [
+                DropdownMenuEntry<String>(label: "20", value: "Teste 1"),
+                DropdownMenuEntry<String>(label: "30", value: "Teste 2"),
+                DropdownMenuEntry<String>(label: "40", value: "Teste 3"),
+                DropdownMenuEntry<String>(label: "Todos", value: "Teste 3"),
+              ],
+            ),
+          ],
+        ),
+        FilledButton(onPressed: () {}, child: Text("Calcular Risco")),
+      ],
+    );
+  }
+}

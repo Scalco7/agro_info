e9a8a2efc617a1cf50706b7a12c4657cf9b16219
Felipe Scalco <fscalco7@gmail.com>
2026-03-11 09:45:15 -0300

    feat: add city model

---
+++ lib/app/common/models/city.dart
@@ -0,0 +1,34 @@
+class City {
+  final int ibgeCode;
+  final String name;
+  final String state;
+  final int latitude;
+  final int longitude;
+  final String soyRegion;
+  final String wheatRegion;
+  final String updatedAt;
+
+  City({
+    required this.ibgeCode,
+    required this.name,
+    required this.state,
+    required this.latitude,
+    required this.longitude,
+    required this.soyRegion,
+    required this.wheatRegion,
+    required this.updatedAt,
+  });
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      ibgeCode: json["codigoIBGE"],
+      name: json["nome"],
+      state: json["uf"],
+      latitude: json["latitude"],
+      longitude: json["longitude"],
+      soyRegion: json["regiaoSoja"],
+      wheatRegion: json["regiaoTrigo"],
+      updatedAt: json["dataAtualizacao"],
+    );
+  }
+}

504bb597ccbc14933e51d261d10272fa2a67bb2e
Felipe Scalco <fscalco7@gmail.com>
2026-03-11 13:52:57 -0300

    feat: crate agritec service

---
+++ lib/app/common/services/agritec_service.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+
+import 'package:agro_info/app/common/enums/api_key_type.dart';
+import 'package:agro_info/app/common/enums/state_enum.dart';
+import 'package:agro_info/app/common/models/agritec_crop.dart';
+import 'package:agro_info/app/common/models/city.dart';
+import 'package:agro_info/app/common/models/zonig_result.dart';
+import 'package:agro_info/app/common/services/api_service.dart';
+import 'package:http/http.dart';
+
+abstract class IAgriTecService {
+  Future<List<City>> getCities(StateEnum state);
+  Future<List<AgritecCrop>> getCropies();
+  Future<ZoningResult> calcZone({
+    required int cultureId,
+    required int ibgeCode,
+    required String risk,
+  });
+}
+
+class AgriTecService implements IAgriTecService {
+  String get _apiUrl => "https://api.cnptia.embrapa.br/agritec/v2";
+  final IApiService apiService = ApiService();
+
+  @override
+  Future<ZoningResult> calcZone({
+    required int cultureId,
+    required int ibgeCode,
+    required String risk,
+  }) async {
+    Uri uri = Uri.parse(
+      "$_apiUrl/zoneamento?idCultura=$cultureId&codigoIBGE=$ibgeCode&risco=$risk",
+    );
+    try {
+      Response response = await apiService.get(
+        uri,
+        apiKeyType: ApiKeyType.embrapa,
+      );
+      var data = jsonDecode(response.body.toString());
+      if (response.statusCode != 200) {
+        throw Exception("Erro ao calcular risco");
+      }
+      ZoningResult zoningResponse = ZoningResult.fromJson(data[0]);
+      return zoningResponse;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<City>> getCities(StateEnum state) async {
+    Uri uri = Uri.parse(
+      "$_apiUrl/municipios?uf=${state.acronym}&dataAtualizacao=2018-01-01",
+    );
+    try {
+      Response response = await apiService.get(
+        uri,
+        apiKeyType: ApiKeyType.embrapa,
+      );
+      var data = jsonDecode(response.body.toString());
+      if (response.statusCode != 200) {
+        throw Exception("Erro ao buscar municipios");
+      }
+      List<City> cities = [];
+
+      for (Map<String, dynamic> index in data) {
+        cities.add(City.fromJson(index));
+      }
+      return cities;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<AgritecCrop>> getCropies() async {
+    Uri uri = Uri.parse("$_apiUrl/culturas");
+    try {
+      Response response = await apiService.get(
+        uri,
+        apiKeyType: ApiKeyType.embrapa,
+      );
+      var data = jsonDecode(response.body.toString());
+      if (response.statusCode != 200) {
+        throw Exception("Erro ao buscar culturas");
+      }
+      List<AgritecCrop> cropies = [];
+
+      for (Map<String, dynamic> index in data) {
+        cropies.add(AgritecCrop.fromJson(index));
+      }
+      return cropies;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}

56821493b90b21fdf3bb730fc37aacb94a20f754
Felipe Scalco <fscalco7@gmail.com>
2026-03-19 10:04:09 -0300

    feat: change plague widget style

--- lib/app/pages/plague/widgets/plague_widget.dart
+++ lib/app/pages/plague/widgets/plague_widget.dart
@@ -9,54 +9,53 @@ class PlagueWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDoenca = plague.classification == PlagueClassification.doenca;
+
+    final backgroundColor = isDoenca
+        ? theme.colorScheme.errorContainer
+        : theme.colorScheme.secondaryContainer;
+
+    final contentColor = isDoenca
+        ? theme.colorScheme.onErrorContainer
+        : theme.colorScheme.onSecondaryContainer;
+
     return AnimatedPadding(
       duration: Duration(milliseconds: 300),
-      curve: Curves.linear,
+      curve: Curves.easeInOut,
       padding: EdgeInsets.all(padding),
-      child: GestureDetector(
-        child: Container(
-          width: 100,
-          height: 80,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            color: plague.classification == PlagueClassification.doenca
-                ? Theme.of(context).colorScheme.errorContainer
-                : Theme.of(context).colorScheme.primaryContainer,
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.16),
-                blurRadius: 6,
-                spreadRadius: 0,
-                offset: Offset(0, 3),
-              ),
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.23),
-                blurRadius: 6,
-                spreadRadius: 0,
-                offset: Offset(0, 3),
-              ),
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(
+          16,
+        ),
+        elevation: 2,
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            width: 100,
+            height: 80,
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              spacing: 4,
+              spacing: 8,
               children: [
                 Icon(
                   plague.icon,
-                  size: 30,
-                  color: Theme.of(context).colorScheme.onPrimaryContainer,
+                  size: 28,
+                  color: contentColor,
                 ),
                 Text(
                   plague.comumName[0],
                   textAlign: TextAlign.center,
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onPrimaryContainer,
+                  style: theme.textTheme.labelMedium?.copyWith(
+                    color: contentColor,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
                   ),
                 ),
               ],

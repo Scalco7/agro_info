ad5a586b13ec7407e2a68ecccd141fabd54a3362
Felipe Scalco <fscalco7@gmail.com>
2026-03-24 11:32:56 -0300

    feat; create market state controller widget

--- lib/app/ui/pages/home/widgets/market/market_state_controller_widget.dart
+++ lib/app/ui/pages/home/widgets/market/market_state_controller_widget.dart
@@ -1,11 +1,27 @@
+import 'package:agro_info/app/common/states/market_state.dart';
+import 'package:agro_info/app/common/viewmodels/home_viewmodel.dart';
 import 'package:agro_info/app/ui/pages/home/widgets/market/market_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MarketStateControllerWidget extends StatelessWidget {
   const MarketStateControllerWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MarketWidget();
+    return Consumer<HomeViewmodel>(
+      builder: (context, homeViewmodel, widget) {
+        IMarketState state = homeViewmodel.marketState;
+
+        switch (state) {
+          case LoadingMarketState():
+            return Center(child: const CircularProgressIndicator());
+          case FailureMarketState():
+            return Center(child: Text(state.errorMessage));
+          case LoadedMarketState():
+            return MarketWidget(commodities: state.commodities);
+        }
+      },
+    );
   }
 }

479e5d03522140e7dd8fe82fb0cc8a9ef137e770
Felipe Scalco <fscalco7@gmail.com>
2026-03-24 11:33:23 -0300

    feat; add market functions to homeViewModel

--- lib/app/common/viewmodels/home_viewmodel.dart
+++ lib/app/common/viewmodels/home_viewmodel.dart
@@ -1,9 +1,13 @@
+import 'package:agro_info/app/common/enums/commodities_enum.dart';
+import 'package:agro_info/app/common/models/market/get_commodity_data_response_model.dart';
 import 'package:agro_info/app/common/models/news/news_model.dart';
 import 'package:agro_info/app/common/models/news/news_response_model.dart';
 import 'package:agro_info/app/common/resources/result.dart';
 import 'package:agro_info/app/common/services/location_service.dart';
+import 'package:agro_info/app/common/services/market_service.dart';
 import 'package:agro_info/app/common/services/news_service.dart';
 import 'package:agro_info/app/common/services/weather_service.dart';
+import 'package:agro_info/app/common/states/market_state.dart';
 import 'package:agro_info/app/common/states/news_state.dart';
 import 'package:agro_info/app/common/states/weather_state.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +16,7 @@ class HomeViewmodel extends ChangeNotifier {
   final IWeatherService _weatherService;
   final ILocationService _locationService;
   final INewsService _newsService;
+  final IMarketservice _marketService;
 
   IWeatherState _weatherState = LoadingWeatherState();
   IWeatherState get weatherState => _weatherState;
@@ -19,18 +24,28 @@ class HomeViewmodel extends ChangeNotifier {
   INewsState _newsState = LoadingNewsState();
   INewsState get newsState => _newsState;
 
+  IMarketState _marketState = LoadingMarketState();
+  IMarketState get marketState => _marketState;
+
   HomeViewmodel({
     required IWeatherService weatherService,
     required ILocationService locationService,
     required INewsService newsService,
+    required IMarketservice marketService,
   }) : _weatherService = weatherService,
        _locationService = locationService,
-       _newsService = newsService {
-    fetchForecasts();
+       _newsService = newsService,
+       _marketService = marketService {
+    _fetchForecasts();
     _initFetchNews();
+    _fetchMarketData();
   }
 
-  void _emit({IWeatherState? weatherState, INewsState? newsState}) {
+  void _emit({
+    IWeatherState? weatherState,
+    INewsState? newsState,
+    IMarketState? marketState,
+  }) {
     if (weatherState != null) {
       _weatherState = weatherState;
     }
@@ -39,10 +54,14 @@ class HomeViewmodel extends ChangeNotifier {
       _newsState = newsState;
     }
 
+    if (marketState != null) {
+      _marketState = marketState;
+    }
+
     notifyListeners();
   }
 
-  void fetchForecasts() async {
+  void _fetchForecasts() async {
     _emit(weatherState: LoadingWeatherState());
 
     Result positionResult = await _locationService.determinePosition();
@@ -148,5 +167,49 @@ class HomeViewmodel extends ChangeNotifier {
   void _initFetchNews() async {
     _emit(newsState: LoadingNewsState());
     _getAggricultureLastNews(1);
+  }
+
+  void _fetchMarketData() async {
+    _emit(marketState: LoadingMarketState());
+
+    List<CommoditiesEnum> commoditiesToFetch = [
+      CommoditiesEnum.aluminum,
+      // CommoditiesEnum.coffee,
+      // CommoditiesEnum.wheat,
+    ];
+
+    for (CommoditiesEnum commodity in commoditiesToFetch) {
+      _marketService
+          .getCommodityData(commodity)
+          .then(
+            (commodityResult) =>
+                _processCommodityResponse(commodity, commodityResult),
+          );
+
+      // Tempo de espera pois a api gratuita só aceita 1 requisição por segundo
+      await Future.delayed(Duration(milliseconds: 2100));
+    }
+  }
+
+  void _processCommodityResponse(
+    CommoditiesEnum commodity,
+    Result<GetCommodityDataResponseModel, Exception> commodityResult,
+  ) {
+    switch (commodityResult) {
+      case Success(value: var data):
+        Map<CommoditiesEnum, GetCommodityDataResponseModel> commodities = {};
+
+        if (_marketState is LoadedMarketState) {
+          commodities = (_marketState as LoadedMarketState).commodities;
+        }
+
+        commodities[commodity] = data;
+
+        _emit(marketState: LoadedMarketState(commodities: commodities));
+        break;
+      case Failure(error: var error):
+        _emit(marketState: FailureMarketState(error.toString()));
+        break;
+    }
   }
 }

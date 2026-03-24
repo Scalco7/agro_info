2cd86979e17f2d6e2f0290c45f2ef8b954dc9a57
Felipe Scalco <fscalco7@gmail.com>
2026-03-24 10:48:50 -0300

    feat: create market_state

--- lib/app/common/states/market_state.dart
+++ lib/app/common/states/market_state.dart
@@ -1,7 +1,14 @@
+import 'package:agro_info/app/common/enums/commodities_enum.dart';
+import 'package:agro_info/app/common/models/market/commodity_data_model.dart';
+
 sealed class IMarketState {}
 
 class LoadingMarketState implements IMarketState {}
 
-class LoadedMarketState implements IMarketState {}
+class LoadedMarketState implements IMarketState {
+ final  Map<CommoditiesEnum, CommodityDataModel> commodities;
+
+ const LoadedMarketState({required this.commodities});
+}
 
 class FailureMarketState implements IMarketState {}

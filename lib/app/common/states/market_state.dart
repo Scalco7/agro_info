0ccb43248d9f9732712125de1e4fffa858e7ea7a
Felipe Scalco <fscalco7@gmail.com>
2026-03-24 11:33:49 -0300

    feat: change market state to get erros

--- lib/app/common/states/market_state.dart
+++ lib/app/common/states/market_state.dart
@@ -1,14 +1,18 @@
 import 'package:agro_info/app/common/enums/commodities_enum.dart';
-import 'package:agro_info/app/common/models/market/commodity_data_model.dart';
+import 'package:agro_info/app/common/models/market/get_commodity_data_response_model.dart';
 
 sealed class IMarketState {}
 
 class LoadingMarketState implements IMarketState {}
 
 class LoadedMarketState implements IMarketState {
- final  Map<CommoditiesEnum, CommodityDataModel> commodities;
+  final Map<CommoditiesEnum, GetCommodityDataResponseModel> commodities;
 
- const LoadedMarketState({required this.commodities});
+  const LoadedMarketState({required this.commodities});
 }
 
-class FailureMarketState implements IMarketState {}
+class FailureMarketState implements IMarketState {
+  final String errorMessage;
+
+  FailureMarketState(this.errorMessage);
+}
